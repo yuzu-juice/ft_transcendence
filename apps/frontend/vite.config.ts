d14ab6e8dcfa8cc2796b51ea8a48ad0d97ec43dc
eno1220 <eno1220dev@gmail.com>
2026-08-29 00:32:47 +0900

    chore: improve ts config ans vite config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
@@ -13,4 +14,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
