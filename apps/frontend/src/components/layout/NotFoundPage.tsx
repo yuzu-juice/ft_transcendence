eccf01bbca15364b96e0b34e985b9e620cbe74a3
eno1220 <eno1220dev@gmail.com>
2026-09-12 03:28:28 +0900

    chore(i18n): multi language support for 404 page

--- apps/frontend/src/components/layout/NotFoundPage.tsx
+++ apps/frontend/src/components/layout/NotFoundPage.tsx
@@ -1,15 +1,18 @@
+import { useTranslation } from 'react-i18next'
 import { CustomLink } from '@/components/ui/CustomLink'
 import { Footer } from '@/components/ui/Footer'
 import { Header } from '@/components/ui/Header'
 
 export const NotFoundPage = () => {
+  const { t } = useTranslation()
+
   return (
     <div className="min-h-dvh w-full flex flex-col gap-8">
       <Header />
       <main className="flex-1 flex flex-col items-center gap-3">
         <h2 className="text-5xl font-bold">404 Not Found</h2>
         <CustomLink to="/" className="text-lg">
-          トップページへ
+          {t('notFound.goToHome')}
         </CustomLink>
       </main>
       <Footer />
